64421d7b53b6108800da3a81011164412e568de9
Randel Jiao <randeljiao@gmail.com>
2019-10-04 23:46:55 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -5,6 +5,9 @@ end
 def shipping_manifest
   the_manifest = { "whale bone corsets" => 5, "procelain vases" => 2, "oil paintings" => 3}
   the_manifest["oil paintings"]
+  the_manifest["muskets"] = 2 
+  the_manifest["gun powder"] = 4
+  puts the_manifest
 end
 
 puts shipping_manifest
