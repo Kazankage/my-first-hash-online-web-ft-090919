aada35172acd344642141857bfee161e7bf2e333
Randel Jiao <randeljiao@gmail.com>
2019-10-04 23:45:32 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -4,6 +4,7 @@ end
 
 def shipping_manifest
   the_manifest = { "whale bone corsets" => 5, "procelain vases" => 2, "oil paintings" => 3}
+  the_manifest["oil paintings"]
 end
+
 shipping_manifest
-the_manifest["oil paintings"]
